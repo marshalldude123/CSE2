6ee2b6ead4c63e7e789f4f0ce80afa7e4d4b2fdc
marshalldude123 <mps219@lehigh.edu>
2016-01-29 16:53:00 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+// Marshall Stallings
+// lab001
+// Hello World Java Program
+//
+// First compile the program
+//          javac HelloWorld.java
+// Run the program
+//          java HelloWorld//
+
+// Define a class
+public class HelloWorld{
+    
+    // Add a main method
+    public static void main(String[] args) {
+        
+        // Print statement with a box
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("#                     #");
+        System.out.println("# Hello World 2016!!! #");
+        System.out.println("#                     #");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("test");
+        
+        
+    }
+}
